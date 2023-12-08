a8be9044815ed50aad89b98aef3a63ce8c7a65de
dgeorgik <gosha_pustovalov@mail.ru>
2023-12-08 03:49:56 +0300

    Add changed README.md % Calculating class

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,30 @@
 public class Calculator {
-    public int add(int a, int b){
-        //TODO inser your realisation in method add
+    public int add(int a, int b) {
+        return a + b;
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+
+    public int dif(int a, int b) {
+        return a - b;
     }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
+
+    public int div(int a, int b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            throw new ArithmeticException("Нельзя делить на 0");
+        }
     }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
+
+    public int times(int a, int b) {
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    public int solver() {
+        int result = add(5, 3);
+        result = times(result, 2);
+        result = dif(result, 3);
+        result = div(result, 4);
+
+        return result;
     }
 }
